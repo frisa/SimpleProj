83f7de0b861af1c576a2f325f2f4fd338fda1712
Jan Fridrichovsky <jan@Jans-MacBook-Air.local>
2023-04-15 22:10:41 +0200

    Qt console application improved

--- myprogram.cpp
+++ myprogram.cpp
@@ -1,9 +1,9 @@
-#include <QCoreApplication>
+#include <QtCore>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::cout << "This is the running console application" << std::endl;
+    std::cout << "This is the running console application with Qt support" << std::endl;
     return a.exec();
 }
